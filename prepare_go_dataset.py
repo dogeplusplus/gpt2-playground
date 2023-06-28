3fb4315b6f400a46040615f7fc40352db45e8f1e
Albert Chung <albertswchung@gmail.com>
2023-06-28 23:58:03 +0100

    feat: Encode go moves using a fixed encoding.

--- prepare_go_dataset.py
+++ prepare_go_dataset.py
@@ -2,10 +2,47 @@ import tiktoken
 import pandas as pd
 import numpy as np
 
-from typing import List
+from ast import literal_eval
+from typing import List, Tuple
 from sgfmill import sgf
 from pathlib import Path
 from rich.progress import track
+
+ALL_MOVES = [
+    (p, (i, j)) for p in ('b', 'w') for i in range(19) for j in range(19)
+] + [('w', None), ('b', None)]
+
+ENCODING = {
+    move: i for i, move in enumerate(ALL_MOVES, 1)
+}
+
+
+def grid_encoding(moves: List[Tuple[str, Tuple[int, int]]]) -> int:
+    moves = literal_eval(moves)
+    moves = [m for m in moves if m[0] is not None]
+    moves_encoded = [ENCODING[m] for m in moves]
+    return moves_encoded
+
+
+def encode_moves_fixed(moves_df: pd.DataFrame, output_file: Path) -> List[int]:
+    moves_df.dropna(subset=["moves", "result"], inplace=True)
+
+    train_df = moves_df.sample(frac=0.9)
+    val_df = moves_df.drop(train_df.index)
+
+    train_df["moves"] = train_df["moves"]
+    val_df["moves"] = val_df["moves"]
+
+    train_ids = [grid_encoding(m) for m in train_df["moves"].to_list()]
+    val_ids = [grid_encoding(m) for m in val_df["moves"].to_list()]
+
+    max_seq_length = max(len(x) for x in train_ids + val_ids)
+    # 0 for padding
+    train_ids = np.array([x + [0] * (max_seq_length - len(x)) for x in train_ids], dtype=np.uint16)
+    val_ids = np.array([x + [0] * (max_seq_length - len(x)) for x in val_ids], dtype=np.uint16)
+
+    np.save(Path(output_file).with_suffix(".train.npy"), train_ids)
+    np.save(Path(output_file).with_suffix(".val.npy"), val_ids)
 
 
 def format_moves(moves: List[str]) -> str:
@@ -39,7 +76,7 @@ def read_sgf_moves(sgf_file: Path):
         result=get_value("RE"),
         rules=get_value("RU"),
         overtime=get_value("OT"),
-        moves=format_moves([node.get_move() for node in main_game[1:]])
+        moves=[node.get_move() for node in main_game],
     )
 
     return parameters
@@ -56,7 +93,7 @@ def export_games_to_csv(sgf_file_path: Path, output_file: Path):
     df.to_csv(output_file, index=False)
 
 
-def encode_moves(moves_df: pd.DataFrame, output_file: Path):
+def encode_moves_nlp(moves_df: pd.DataFrame, output_file: Path):
     enc = tiktoken.get_encoding("gpt2")
     moves_df.dropna(subset=["moves", "result"], inplace=True)
 
@@ -86,7 +123,7 @@ def main():
         export_games_to_csv(sgf_file_path, output_file)
 
     encoded_file = "go.bin"
-    encode_moves(pd.read_csv(output_file), encoded_file)
+    encode_moves_fixed(pd.read_csv(output_file), encoded_file)
 
 
 if __name__ == "__main__":
