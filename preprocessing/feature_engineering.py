fix: set 0 tokens to -1 in the loss calculation for masking.

preprocessing/feature_engineering.py
@@ -1,3 +1,4 @@
+import re
 import tiktoken
 import numpy as np
 import pandas as pd
@@ -16,12 +17,10 @@ ALL_MOVES = [" "] + [
 ENCODING = {
     move: i for i, move in enumerate(ALL_MOVES)
 }
-ENCODING["[PAD]"] = -1
 
 DECODING = {
     i: move for i, move in enumerate(ALL_MOVES)
 }
-DECODING[-1] = "[PAD]"
 
 
 def board_state(moves: List[Tuple[str, Tuple[int, int]]]) -> np.ndarray:
@@ -51,7 +50,7 @@ def encode_moves_fixed(moves_df: pd.DataFrame, output_file: Path, max_seq_length
     train_ids = [grid_encoding(m) for m in train_df["moves"].to_list()]
     val_ids = [grid_encoding(m) for m in val_df["moves"].to_list()]
 
-    pad_token = ENCODING["[PAD]"]
+    pad_token = 0
     # -1 token for padding
     train_ids = np.array(
         [x + [pad_token] * (max_seq_length - len(x))
@@ -97,3 +96,20 @@ def extract_features(example: Dict[str, Any]):
     example["moves"] = grid_encoding(example["moves"])
 
     return example
+
+
+def process_result(game: Dict[str, Any]):
+    outcome = {
+        "B": 0,
+        "W": 1,
+        "d": 2,
+    }
+    pattern = r"([+-]?\d+(\.\d+)?)"
+    result = re.findall(pattern, game["result"])
+    game["result"] = outcome[game["result"][0]]
+    if result:
+        result = result[0][0]
+        game["point_difference"] = float(result)
+    else:
+        game["point_difference"] = None
+    return game